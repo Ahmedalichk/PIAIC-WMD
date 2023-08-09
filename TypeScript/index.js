cdfac9b71166918706c35d3b038172ea8da9d7b1
Ahmed Ali <ahmedalichk@gmail.com>
2023-08-09 16:54:49 +0500

    Add files via upload

---
+++ TypeScript/index.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+let myName = "Ahmed"; //string dataType
+
+//let, const, var
+
+let age = 32;
+
+let address = "ISB";
+
+console.log("My Name is: ", myName);
+console.log("My Age is: ", age);
+console.log("Address is: ", address);
+
+
